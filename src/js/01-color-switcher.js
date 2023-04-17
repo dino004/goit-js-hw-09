e8931a5ea229f595cfe6f7e243467e660609ca52
Dmytro Lytvyn <dmytro4lytvyn@gmail.com>
2023-04-17 22:40:13 +0300

    Виконав перше завдання

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
+const body = document.querySelector('body');
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+let timerId;
+
+startBtn.addEventListener('click', getColor);
+
+function getColor() {
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+  body.style.backgroundColor = getRandomHexColor();
+
+  timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+stopBtn.addEventListener('click', stopColor);
+
+function stopColor() {
+  clearInterval(timerId);
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+}
