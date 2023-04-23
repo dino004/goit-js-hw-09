f13323770adaa0e22068fd507a52da620861f280
dino004 <109174001+dino004@users.noreply.github.com>
2023-04-23 06:00:40 +0000

    Deploying to gh-pages from @ dino004/goit-js-hw-09@3d0ec5a7970411880b549b026a26e36243ce5516 🚀

---
+++ 01-color-switcher.c4ab9cae.js
@@ -0,0 +1,2 @@
+!function(){function t(){return"#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}var e=document.querySelector("body"),n=document.querySelector("button[data-start]"),o=document.querySelector("button[data-stop]"),a=null;n.addEventListener("click",(function(){n.disabled=!0,o.disabled=!1,e.style.backgroundColor=t(),a=setInterval((function(){e.style.backgroundColor=t()}),1e3)})),o.addEventListener("click",(function(){clearInterval(a),n.disabled=!1,o.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.c4ab9cae.js.map
