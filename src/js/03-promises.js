5f3cc5c58a0cc699d4cd7e844dc39ed0041334a7
Dmytro Lytvyn <dmytro4lytvyn@gmail.com>
2023-04-30 18:31:12 +0300

    Почав виконувати третє завдання

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,38 @@
+// Отримуємо доступ до елементів
+const refs = {
+  firstDelay: document.querySelector('input[name=delay]'),
+  delayStep: document.querySelector('input[name=step]'),
+  amount: document.querySelector('input[name=amount]'),
+  button: document.querySelector('button'),
+};
+
+// Функція створення промісів
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setInterval(() => {
+      if (shouldResolve) {
+        resolve(`Fulfilled promise ${position} in ${delay}ms`);
+      } else {
+        reject(`Reject promise ${position} in ${delay}ms`);
+      }
+    });
+  });
 }
+
+let i = 1;
+
+function position() {
+  i++;
+}
+
+function delay() {
+  refs.firstDelay.value;
+}
+
+// Виклик сабміта
+refs.button.addEventListener('click', e => {
+  e.preventDefault();
+  createPromise(refs.amount.value, refs.firstDelay.value).then(resolve).catch(reject);
+});
